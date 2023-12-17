b483893e56410958a0440fc5e78693f86b9a5258
DevAnsar <ansarmirzayi@gmail.com>
2023-12-17 16:19:13 +0330

    init react app with react-router

---
+++ src/router.ts
@@ -0,0 +1,14 @@
+import { RouteObject, createBrowserRouter } from "react-router-dom";
+// import ErrorPage from './pages/layouts/ErrorPage';
+
+const routes: Array<RouteObject> = [
+  {
+    path: "/",
+    // errorElement: <ErrorPage />,
+  },
+  {
+    path: "*",
+    // element: <ErrorPage />,
+  },
+];
+export const router = createBrowserRouter(routes);
